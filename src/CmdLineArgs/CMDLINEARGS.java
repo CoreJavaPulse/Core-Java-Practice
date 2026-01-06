e609e4a081c434175a0db0ea045366f6acafe3fb
Hitesh Mane <HiteshMane5hm@gmail.com>
2026-01-06 17:45:10 +0530

    sum of array elements using CMD Line Arguments.

--- src/CmdLineArgs/CMDLINEARGS.java
+++ src/CmdLineArgs/CMDLINEARGS.java
@@ -4,12 +4,18 @@ public class CMDLINEARGS {
 
 	public static void main(String[] args) {
 		
-		String s1  = args[0];
-		String s2  = args[1];
-		int n1 = Integer.parseInt(s1);
-		int n2 = Integer.parseInt(s2);
-		int add =n1+n2;
-		System.out.println("Addition Is :"+add);
+//		String s1  = args[0];
+//		String s2  = args[1];
+//		int n1 = Integer.parseInt(s1);
+//		int n2 = Integer.parseInt(s2);
+//		int add =n1+n2;
+//		
+		int sum=0;
+		for(int i = 0;i<args.length;i++)
+		{
+			sum += Integer.parseInt(args[i]);
+		}
+		System.out.println("Addition Is :"+sum);
 
 	}
 
